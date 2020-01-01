124ea6b503e34b77298e78852a2d0fd5b12e5122
Cipta Ageung Mahdiar <cipta_ageung@yahoo.co.id>
2020-01-01 21:07:03 +0700

    Update auth.go

--- services/auth.go
+++ services/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	auth "github.com/cipta-ageung/simas-user/proto/auth"
+	empty "github.com/golang/protobuf/ptypes/empty"
 )
 
 // AuthService : struct interface
@@ -15,22 +16,37 @@ func (g *AuthService) Auth(ctx context.Context, req *auth.User, rsp *auth.Token)
 }
 
 // Create : method create user
-func (g *AuthService) Create(ctx context.Context, req *auth.User, rsp *auth.Response) error {
+func (g *AuthService) Create(ctx context.Context, req *auth.CreateUserRequest, rsp *auth.CreateUserResponse) error {
 	return nil
 }
 
 // Delete : method delete user
-func (g *AuthService) Delete(ctx context.Context, req *auth.User, rsp *auth.Response) error {
+func (g *AuthService) Delete(ctx context.Context, req *auth.DeleteUserRequest, rsp *auth.DeleteUserResponse) error {
 	return nil
 }
 
-// Get : method get user
-func (g *AuthService) Get(ctx context.Context, req *auth.User, rsp *auth.Response) error {
+// Read : method get user
+func (g *AuthService) Read(context.Context, *auth.ReadUserRequest, *auth.ReadUserResponse) error {
 	return nil
 }
 
-// GetAll : method get all user
-func (g *AuthService) GetAll(ctx context.Context, req *auth.Request, rsp *auth.Response) error {
+// Update : method update user
+func (g *AuthService) Update(context.Context, *auth.UpdateUserRequest, *auth.UpdateUserResponse) error {
+	return nil
+}
+
+// UpdateSet : method update list user
+func (g *AuthService) UpdateSet(context.Context, *auth.UpdateSetUserRequest, *auth.UpdateSetUserResponse) error {
+	return nil
+}
+
+// List : method get all user
+func (g *AuthService) List(context.Context, *auth.ListUserRequest, *auth.ListUserResponse) error {
+	return nil
+}
+
+// CustomMethod : custom method
+func (g *AuthService) CustomMethod(context.Context, *empty.Empty, *empty.Empty) error {
 	return nil
 }
 
