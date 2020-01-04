cc21a12159b6fa7e4ccc3c1d971ad915fd58a012
cipta-ageung <cipta_ageung@yahoo.co.id>
2020-01-04 10:04:38 +0700

    update connect dv

--- main.go
+++ main.go
@@ -4,14 +4,46 @@ import (
 	"log"
 	"time"
 
+	"context"
+
+	_ "github.com/jinzhu/gorm"
+
+	// driver postgres
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	cfg "github.com/cipta-ageung/simas-db/config"
+	pgdb "github.com/cipta-ageung/simas-db/proto"
 	auth "github.com/cipta-ageung/simas-user/proto/auth"
 	services "github.com/cipta-ageung/simas-user/services"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/service/grpc"
 )
 
 func main() {
 
 	log.Println("starting services user . . .")
+
+	serviceClient := grpc.NewService()
+	serviceClient.Init()
+
+	// use the generated client stub
+	serviceDb := pgdb.NewServiceConnectionService("go.micro.srv.simasdb", serviceClient.Client())
+	if serviceDb != nil {
+		log.Println("fuck")
+	}
+	svcDb, err := serviceDb.SetupDb(context.TODO(), &pgdb.ServiceApp{Svc: "go.micro.srv.simasuser"})
+	if err != nil || svcDb == nil {
+		log.Fatalf("cannot setup database")
+	}
+
+	db, err := cfg.Connect(svcDb.ConnectionDb)
+	if err != nil {
+		log.Fatal("cannot connet database")
+	}
+
+	ctx := context.WithValue(context.Background(), "db", db)
+
+	log.Println(ctx)
 
 	service := micro.NewService(
 		micro.Name("go.micro.srv.simasuser"),
